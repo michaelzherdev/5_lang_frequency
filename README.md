204dbcbb2c627e10f9abb69b8cf2437bb9ee6ce8
Michael1987 <michaelzherdev@gmail.com>
2018-02-25 21:39:26 +0300

    no user input, use args, readme

--- README.md
+++ README.md
@@ -2,6 +2,28 @@
 
 The program searched for 10 most frequent words in received text.
 
+# How to use
+Run from command line:
+```shell
+cd ${path_to_script}
+python lang_frequency.py d:\text.txt
+```
+Example of output:
+
+```shell
+WORD : NUMBER OF TIMES
+encoding : 10
+the : 10
+to : 6
+string : 4
+python : 4
+in : 3
+be : 3
+utf : 3
+use : 3
+for : 3
+```
+
 # How to run.
 
 For correct work of script Python 3.5 is required.
@@ -11,6 +33,8 @@ Run on Linux/Windows:
 ```bash
 
 $ python lang_frequency.py # possibly requires call of python3 executive instead of just python
+
+```
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,25 +1,29 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import os
 import re
+from sys import argv
 from collections import Counter
 
-NUMBER_OF_MOST_FREQUENT_WORDS = 10
 
 def load_data(filepath):
-    with open(os.path.abspath(filepath), encoding="utf8") as f:
+    with open(filepath, encoding="utf8") as f:
         text = f.read()
     return text
 
 
 def get_most_frequent_words(text):
-    words = re.findall("([\w][\w]*'?\w?)", text.lower())
-    return Counter(words).most_common(NUMBER_OF_MOST_FREQUENT_WORDS)
+    NUMBER_OF_MOST_FREQUENT_WORDS = 10
+    all_words = re.findall("([\w][\w]*'?\w?)", text.lower())
+    return Counter(all_words).most_common(NUMBER_OF_MOST_FREQUENT_WORDS)
 
 
-if __name__ == '__main__':
-    filepath = input("Путь к текстовому файлу: ")
-    text = load_data(filepath)
-    most_frequent_words = get_most_frequent_words(text=text)
-    print(most_frequent_words)
+if __name__ == "__main__":
+    try:
+        text = load_data(argv[1])
+        most_frequent_words = get_most_frequent_words(text)
+        print("WORD : NUMBER OF TIMES")
+        for word, number_of_times in most_frequent_words:
+            print("{} : {}".format(word, number_of_times))
+    except IndexError:
+        print("Please, enter path to text file. See README.md.")
+    except FileNotFoundError:
+        print("No such file or directory. Please provide a valid file path.")
